5b72ffdb3b3df47cb28875b7a279be35eb1b44f9
Boominathan <eboominathan@gmail.com>
2024-04-28 05:06:12 +0530

    home page in progress

---
+++ resources/js/Pages/ChatLayout.jsx
@@ -0,0 +1,19 @@
+ import { usePage } from '@inertiajs/react';
+const ChatLayout = ({children}) => {
+    const page = usePage();
+    const conversations = page.props.conversations;
+    const selectedConversations = page.props.selectedConversations;
+    console.log(conversations,'conversations');
+    console.log(selectedConversations,'selectedConversations');
+
+  return (
+    <>
+        ChatLayout
+        <div>{children}</div>
+
+    </>
+  )
+}
+ 
+
+export default ChatLayout
