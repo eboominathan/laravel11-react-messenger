3bd75b7c52cb549208d520456106b4b0b7ba9fdf
Boominathan <eboominathan@gmail.com>
2024-04-27 18:02:23 +0530

    factory added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+      
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'John doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
         ]);
+        User::factory()->create([
+            'name' => 'Jane doe',
+            'email' => 'jane@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
+        ]);
+
+        User::factory(10)->create();
+        for ($i=0; $i < 5 ; $i++) { 
+
+            $group = Group::factory()->create([
+                    'owner_id' => 1
+            ]);
+            $users = User::inRandomOrder()->limit(rand(2,5));
+
+            $group->users()->attach([1,...$users]);
+            
+        }
     }
 }
