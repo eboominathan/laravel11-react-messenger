57723ff3f36fdd16ed1e9edabc91051e7a8c72b1
Boominathan <eboominathan@gmail.com>
2024-04-29 07:39:32 +0530

    reverb implementation started

--- resources/js/Pages/ChatLayout.jsx
+++ resources/js/Pages/ChatLayout.jsx
@@ -1,19 +1,57 @@
- import { usePage } from '@inertiajs/react';
-const ChatLayout = ({children}) => {
+import { usePage } from "@inertiajs/react";
+import { useEffect } from "react";
+const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversations = page.props.selectedConversations;
-    console.log(conversations,'conversations');
-    console.log(selectedConversations,'selectedConversations');
+    const [localConversations,setLocalConversations] = page.props.selectedConversations;
 
-  return (
-    <>
-        ChatLayout
-        <div>{children}</div>
+    const [onlineUsers, setOnlineUsers] = useState({});
+    const isUserOnline = (userId) => onlineUsers[userId];
 
-    </>
-  )
-}
- 
+    console.log(conversations, "conversations");
+    console.log(selectedConversations, "selectedConversations");
 
-export default ChatLayout
+    useEffect(() => {
+        Echo.join("online")
+            .here((users) => {
+                const onlineUsersObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+
+                setOnlineUsers((prevOnlineUsers) => {
+                    return { ...prevOnlineUsers, ...onlineUsersObj };
+                });
+            })
+            .joining((user) => {
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updateUsers = { ...prevOnlineUsers };
+                    updateUsers[user.id] = user;
+                    return updateUsers;
+                });
+            })
+            .leaving((user) => {
+                setOnlineUsers((prevOnlineUsers) => {
+                    const updateUsers = { ...prevOnlineUsers };
+                    delete updateUsers[user.id];
+                    return updateUsers;
+                });
+            })
+            .error((error) => {
+                console.log("error", error);
+            });
+
+        return () => {
+            Echo.leave("online");
+        };
+    }, []);
+
+    return (
+        <>
+            ChatLayout
+            <div>{children}</div>
+        </>
+    );
+};
+
+export default ChatLayout;
