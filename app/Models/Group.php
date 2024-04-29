94d04a4c6cde6976a511a6ec6b03a70e298f2e49
Boominathan <eboominathan@gmail.com>
2024-04-29 22:22:38 +0530

    toArray in user,conversation.message added

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -27,4 +27,28 @@ class Group extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function getGroupsForUser(Group $exceptUser)
+    {
+        $query = self::select(['groups.*','messages.message as last_message','messages.created_at as last_message_date'])
+        ->join('group_users','group_users.group_id','=','groups.id')
+        ->leftjoin('messages','message.id','=','group.last_message_id')
+        ->where('group_users.user_id',$user->id)
+        ->orderBy('messages.created_at','desc')
+        ->orderBy('groups.name');
+        return $query->get();
+
+    }
+    public function toConversationArray(){
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'is_group' => true,
+            'is_user' => false,
+            'owner_id' => $this->owner_id,
+            'users' => $this->users,
+            'last_message' => $this->last_message,
+            'last_message_date' => $this->last_message_date,
+        ]
+    }
 }
--- resources/js/Pages/ChatLayout.jsx
+++ resources/js/Pages/ChatLayout.jsx
@@ -4,13 +4,40 @@ const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversations = page.props.selectedConversations;
-    const [localConversations,setLocalConversations] = page.props.selectedConversations;
+    const [localConversations, setLocalConversations] = useState([]);
+    const [sortedConversations, setSortedConversations] = useState([]);
 
     const [onlineUsers, setOnlineUsers] = useState({});
     const isUserOnline = (userId) => onlineUsers[userId];
 
     console.log(conversations, "conversations");
     console.log(selectedConversations, "selectedConversations");
+    useEffect(() => {
+        setSortedConversations(
+            localConversations.sort((a, b) => {
+                if (a.blocked_at && b.blocked_at) {
+                    return a.blocked_at > b.blocked_at ? 1 : -1;
+                } else if (a.blocked_at) {
+                    return 1;
+                } else if (b.blocked_at) {
+                    return 1;
+                }
+                if (a.last_message_at && b.last_message_at) {
+                    return b.last_message.localCompare(a.last_message_at);
+                } else if (a.last_message) {
+                    return -1;
+                } else if (b.last_message) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+        );
+    }, [localConversations]);
+
+    useEffect(() => {
+        setLocalConversations(conversations);
+    }, [localConversations]);
 
     useEffect(() => {
         Echo.join("online")
