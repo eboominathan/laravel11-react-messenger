63970b1642700a9a3c3e7186532e788dafa90dd6
Boominathan <eboominathan@gmail.com>
2024-05-03 07:44:37 +0530

    setting up message resource and controller

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Resources;
 
+use App\Models\MessageAttachment;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class MessageResource extends JsonResource
 {
+    public static $wrap = false;
     /**
      * Transform the resource into an array.
      *
@@ -14,6 +16,17 @@ class MessageResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return  [
+            'id' => $this->id,
+            'message' => $this->message,
+            'sender_id' => $this->sender_id,
+            'receiver_id' => $this->receiver_id,
+            'sender' => new UserResource($this->sender),
+            'group_id' => $this->group_id,
+            'attachments' => MessageAttachmentResource::collection($this->attachments),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -8,12 +9,13 @@ use Inertia\Inertia;
 Route::middleware(['auth','verified'])->group(function(){
     Route::get('/',[HomeController::class,'home'])->name('dashboard');
     
-    Route::get('user/{user}',function(){
-        
-    })->name('chat.user');
-    Route::get('group/{group}',function(){
+    Route::get('user/{user}',[MessageController::class,'byUser'])->name('chat.user');
+    Route::get('group/{group}',[MessageController::class,'byGroup'])->name('chat.group');
 
-    })->name('chat.group');
+    Route::post('/message',[MessageController::class,'store'])->name('message.store');
+    Route::delete('/message/{message}',[MessageController::class,'destroy'])->name('message.destroy');
+
+    Route::get('/message/older/{message}',[MessageController::class,'loadOlder'])->name('message.loadOlder');
 });
  
 
